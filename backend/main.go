11a1ff06c179f9b6e83db7a96b4ddd25d7fa145c
tommivk <tommikokk@gmail.com>
2023-07-01 19:58:40 +0300

    add movies/id route

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -21,7 +22,7 @@ type Movie struct {
 	Id            int
 	Title         string
 	BackdropPath  string `json:"backdrop_path"`
-	GenreIds      []int  `json:"genre_ids"`
+	GenreIds      []int  `json:"genre_ids,omitempty"`
 	Language      string `json:"original_language"`
 	OriginalTitle string `json:"original_title"`
 	Overview      string
@@ -38,8 +39,11 @@ type SearchResult struct {
 
 func fetchData(url string) ([]byte, error) {
 	res, err := http.Get(url)
-	if err != nil || res.StatusCode < 200 || res.StatusCode > 299 {
+	if err != nil {
 		return nil, err
+	}
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return nil, errors.New("Failed to fetch")
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
@@ -47,6 +51,32 @@ func fetchData(url string) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+func getMovieById(c *gin.Context) {
+	id := c.Param("id")
+
+	baseURL, _ := url.Parse("https://api.themoviedb.org/3/movie/" + id)
+	params := url.Values{}
+	params.Add("api_key", API_KEY)
+	baseURL.RawQuery = params.Encode()
+
+	res, err := fetchData(baseURL.String())
+	if len(res) == 0 {
+		c.AbortWithStatus(404)
+		return
+	}
+	if err != nil {
+		c.AbortWithStatus(500)
+		return
+	}
+	var data Movie
+	err = json.Unmarshal(res, &data)
+	if err != nil {
+		c.AbortWithStatus(500)
+		return
+	}
+	c.JSON(http.StatusOK, data)
 }
 
 func searchMovie(c *gin.Context) {
@@ -79,6 +109,6 @@ func main() {
 
 	router.GET("/ping", ping)
 	router.GET("/movies/search", searchMovie)
-
+	router.GET("/movies/:id", getMovieById)
 	router.Run(":8080")
 }

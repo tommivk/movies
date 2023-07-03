2708f71bf71fab99ec2b9997eb9924b01e849e4e
tommivk <tommikokk@gmail.com>
2023-07-03 22:24:50 +0300

    add default values

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -3,18 +3,23 @@ import "./movieCard.scss";
 
 type Props = { movie: Movie };
 
+const placeHolderImg =
+  "https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/story_full_width/8u3k0zn66i_Large_WW170579.jpg";
+
 const MovieCard = ({ movie }: Props) => {
   const imagePath = movie.posterPath ?? movie.backdropPath;
-  const bgImage = imagePath
+  const image = imagePath
     ? `https://image.tmdb.org/t/p/original/${imagePath}`
-    : "https://files.worldwildlife.org/wwfcmsprod/images/Panda_in_Tree/story_full_width/8u3k0zn66i_Large_WW170579.jpg";
+    : placeHolderImg;
 
-  const year = new Date(movie.releaseDate).getFullYear();
+  const year = movie.releaseDate
+    ? new Date(movie.releaseDate).getFullYear()
+    : undefined;
 
   return (
     <div className="card">
       <div className="card__imageWrapper">
-        <img className="card__image" src={bgImage} alt={movie.title}></img>
+        <img className="card__image" src={image} alt={movie.title}></img>
         <div className="card__fade" />
         <h2 className="card__title">{movie.title}</h2>
       </div>

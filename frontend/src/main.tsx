d9258cbe00cb539634072d792f9092007f7d2e77
tommivk <tommikokk@gmail.com>
2023-07-04 21:31:11 +0300

    import global css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+
 import "./index.scss";
+import "./sassStyles/_global.scss";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();

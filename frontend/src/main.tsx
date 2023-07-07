b55c2dfcc4a4f447e234afdd8aa616bc884cc572
tommivk <tommikokk@gmail.com>
2023-07-07 20:40:20 +0300

    sort imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.scss";
 import "./sassStyles/_global.scss";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 

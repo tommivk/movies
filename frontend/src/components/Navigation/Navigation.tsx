ef17b72021bc750240b88b4e6afb3b664cc0ed0b
tommivk <tommikokk@gmail.com>
2023-07-08 23:21:15 +0300

    add navbar

---
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,54 @@
+import Login from "../Login/Login";
+import useAppStore from "../../store";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+import "./navigation.scss";
+
+const Navigation = () => {
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+
+  const store = useAppStore();
+  const setLoggedUser = store.setLoggedUser;
+
+  const handleLogOut = () => {
+    setLoggedUser(undefined);
+    localStorage.removeItem("loggedUser");
+    toast.success("Goodbye");
+  };
+
+  console.log(store.loggedUser);
+
+  return (
+    <div className="nav">
+      <Login modalOpen={loginModalOpen} setModalOpen={setLoginModalOpen} />
+      <ul>
+        <li>
+          <Link to="/">
+            <h1 className="nav__title">Mövies</h1>
+          </Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          {store.loggedUser ? (
+            <button className="btn btn--transparent" onClick={handleLogOut}>
+              Logout
+            </button>
+          ) : (
+            <button
+              className="btn btn--transparent"
+              onClick={() => setLoginModalOpen(true)}
+            >
+              Login
+            </button>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;

71a570d172c4366da0fe6f2d79405eebdf2b224e
tommivk <tommikokk@gmail.com>
2023-08-23 07:34:24 +0300

    add notifications

--- backend/models/recommendations.go
+++ backend/models/recommendations.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"movies/enums"
 	"movies/utils"
 
 	"github.com/gin-gonic/gin"
@@ -16,10 +17,31 @@ type Recommendation struct {
 	Timestamp   string `json:"timestamp"`
 }
 
+func getGroupMembers(tx *sqlx.Tx, groupId, userId int) []int {
+	var userIds []int
+	sql := `SELECT U.id FROM UserGroups UG JOIN Users U ON UG.user_id = U.id WHERE UG.group_id=$1 AND UG.user_id != $2`
+	tx.Select(&userIds, sql, groupId, userId)
+	return userIds
+}
+
+func sendNotificationsToGroupMembers(tx *sqlx.Tx, userIds []int, groupId int) {
+	sql := `INSERT INTO Notifications (user_id, fired_by, notification_type) VALUES ($1, $2, $3)`
+	for _, userId := range userIds {
+		tx.Exec(sql, userId, groupId, enums.NewMovieRecommendation.ToString())
+	}
+}
+
 func (r *Recommendation) AddRecommendation(c *gin.Context, userId, movieId, groupId int, description string) error {
 	db := c.MustGet("db").(*sqlx.DB)
+
+	tx := db.MustBegin()
 	sql := `INSERT INTO Recommendations (movie_id, group_id, user_id, description) VALUES ($1, $2, $3, $4)`
-	_, err := db.Exec(sql, movieId, groupId, userId, utils.NewNullString(description))
+	tx.Exec(sql, movieId, groupId, userId, utils.NewNullString(description))
+
+	groupMembers := getGroupMembers(tx, groupId, userId)
+	sendNotificationsToGroupMembers(tx, groupMembers, groupId)
+
+	err := tx.Commit()
 	if err != nil {
 		return err
 	}
